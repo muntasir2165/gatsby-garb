7b77bded6aa83b493304e5e75b6b89d921994cec
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-27 10:23:38 -0700

    v-12 adding / querying for custom fields on site metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,6 +19,7 @@ const Layout = ({ children }) => {
         siteMetadata {
           title
           author
+          createdAt
         }
       }
     }
@@ -36,7 +37,8 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built by {data.site.siteMetadata.author}
+          Built by {data.site.siteMetadata.author}, Created{" "}
+          {data.site.siteMetadata.createdAt}
         </footer>
       </div>
     </>
