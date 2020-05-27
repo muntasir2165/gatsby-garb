a84752d2f52eb960e781c338e148d2d1de101d9a
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-27 11:18:02 -0700

    v-14 executing static queries from scratch

--- src/pages/posts/page-3.js
+++ src/pages/posts/page-3.js
@@ -1,11 +1,49 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 import Layout from "../../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>Hello from Page 3!</h1>
-    <Link to="/page-2">Go to page 2</Link>
-  </Layout>
-)
+export default () => {
+  // const getImageData = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
+    {
+      allFile {
+        edges {
+          node {
+            relativePath
+            size
+            extension
+            birthTime
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <h1>Hello from Page 3!</h1>
+      <h3>Image File Data</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Relative Path</th>
+            <th>Size of Image</th>
+            <th>Extension</th>
+            <th>Birthtime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map(({ node }, index) => (
+            <tr key={index}>
+              <td>{node.relativePath}</td>
+              <td>{node.size}</td>
+              <td>{node.extension}</td>
+              <td>{node.birthTime}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Link to="/page-2">Go to page 2</Link>
+    </Layout>
+  )
+}
