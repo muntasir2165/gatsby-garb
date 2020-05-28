77822ecc9f30d5bafdba3cb81f35a261a39e5682
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-27 21:45:46 -0700

    v-20 fetching individual post data by slug with page queries

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,9 +1,26 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-export default () => (
+const PostTemplate = ({ data: post }) => (
   <Layout>
-    <h2>Programmatically created blog post!</h2>
+    <div>
+      <h1>{post.markdownRemark.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }}></div>
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+
+export default PostTemplate
