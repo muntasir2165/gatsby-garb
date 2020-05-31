c864b7307e0c3dde20e02d38a3668717a29f6653
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-31 12:16:21 -0700

    v-24 sorting, filtering, limiting, skipping w/ graphql arguments / listing blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,7 +28,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(limit: 1000) {
         edges {
           node {
             fields {
