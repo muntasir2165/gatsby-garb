f46cae484adae380437f8700ba8a87cce4a331ee
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-31 19:49:41 -0700

    v-33 embedding images in markdown files with gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,10 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: "gatsby-transformer-remark",
+      options: { plugins: [{ resolve: "gatsby-remark-images" }] },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/posts/post-two.md
+++ src/posts/post-two.md
@@ -4,3 +4,5 @@ date: "2019-01-15"
 ---
 
 You're currently reading the second post I've made.
+
+![Tranquil Beach](tranquil-beach.jpeg)
